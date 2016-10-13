8afcafee9381f868ad7b17f3624ec0c9ebebffd8
Chris Cordova <cordovac89@gmail.com>
2016-10-13 17:40:08 +1100

    .gitignore is edited and up to date

---
+++ F1WebAPI/Controllers/ScrapeHistoryController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using F1WebAPI.Models;
+using NanoApi;
+using F1WebAPI.ActionFilters;
+
+namespace F1WebAPI.Controllers
+{
+    [CustomAuthorize]
+    [RoutePrefix("api/scrapelog")]
+    public class ScrapeHistoryController : ApiController
+    {
+        [HttpGet()]
+        [Route("get/{type}")]
+        public ApiResponse GetScrapeLastUpdated(string type)
+        {
+            try
+            {
+                var db = NanoApi.JsonFile<ScrapeHistory.Log>.GetInstance(AppDomain.CurrentDomain.BaseDirectory + "\\Resources\\", "scrape_history.json");
+                var driversLastUpdated = db.Select(s => s.scrapename == type);
+            }
+            catch (Exception ex)
+            {
+                return new ApiResponse() { Success = false, ErrorMessage = ex.Message };
+            }
+            
+
+            return new ApiResponse() { Success = true };
+        }
+
+        [HttpPost()]
+        [Route("update/{type}")]
+        public ApiResponse UpdateScrapeLastUpdated(string type)
+        {
+            try
+            {
+                var db = NanoApi.JsonFile<ScrapeHistory.Log>.GetInstance(AppDomain.CurrentDomain.BaseDirectory + "\\Resources\\", "scrape_history.json");
+                db.Update(p => p.scrapename == type, p => p.lastupdated = DateTime.Now.ToString());
+            }
+            catch (Exception ex)
+            {
+                return new ApiResponse() { Success = false, ErrorMessage = ex.Message };
+            }
+            
+
+            return new ApiResponse() { Success = true };
+        }
+
+
+    }
+}
---
+++ F1WebAPI/Models/ScrapeHistory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace F1WebAPI.Models
+{
+    public class ScrapeHistory
+    {
+        public class Log
+        {
+            public int id { get; set; }
+            public string scrapename { get; set; }
+            public string lastupdated { get; set; }
+        }
+    }
+}
